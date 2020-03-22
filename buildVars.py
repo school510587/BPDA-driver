14a06e3d25320e6171cc6aafeabaea5fa0313a6f
Bo-Cheng Jhan <school510587@yahoo.com.tw>
2020-03-22 19:05:02 +0800

    Update addon_minimumNVDAVersion and addon_lastTestedNVDAVersion
    Support of NVDA 2019.3 from now on.

--- buildVars.py
+++ buildVars.py
@@ -27,9 +27,9 @@ addon_info = {
     # Documentation file name
     "addon_docFileName" : "readme.html",
     # Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-    "addon_minimumNVDAVersion" : "2012.2",
+    "addon_minimumNVDAVersion" : "2017.3",
     # Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-    "addon_lastTestedNVDAVersion" : "2019.1.1",
+    "addon_lastTestedNVDAVersion" : "2019.3.1",
     # Add-on update channel (default is stable or None)
     "addon_updateChannel" : None,
 }
